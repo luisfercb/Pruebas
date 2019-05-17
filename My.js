0e474a1bedf428a19bb876d5657ae0459ca85a22
luis Fernando Cardenas <luisfercb@gmail.com>
2019-05-16 20:10:54 -0500

    Cambios Borrados

--- My.js
+++ My.js
@@ -1,6 +1,4 @@
-// Evento en cambio de color
 var elColor = document.getElementById("esteColor");
-// Evento en click del boton
 var elBoton = document.getElementById("boton");
 
 elBoton.addEventListener("click", funcionEscriba);
@@ -9,5 +7,3 @@ function funcionEscriba() {
   var color = elColor.value;
 
   alert("Pase!! " + color);
-}
-

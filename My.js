5dff20b8cf7a2b8490bff34e395f23c697260214
luis Fernando Cardenas <luisfercb@gmail.com>
2019-05-17 08:50:43 -0500

    Comentario de la funcion

--- My.js
+++ My.js
@@ -4,8 +4,9 @@ var elColor = document.getElementById("esteColor");
 var elBoton = document.getElementById("boton");
 // Se adiciono el EventListener
 elBoton.addEventListener("click", funcionEscriba);
-
+// Función de verificación
 function funcionEscriba() {
   var color = elColor.value;
 
   alert("Pase!! " + color);
+}

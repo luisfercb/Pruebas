45a89ba0d347703d971f3d79eeef53b13908c2ba
luis Fernando Cardenas <luisfercb@gmail.com>
2019-05-17 08:19:46 -0500

    Adicion de comentarios

--- My.js
+++ My.js
@@ -1,5 +1,6 @@
+// Evento de color
 var elColor = document.getElementById("esteColor");
-
+// Evento de click en boton
 var elBoton = document.getElementById("boton");
 
 elBoton.addEventListener("click", funcionEscriba);

e7f9ea60ca6769656841698df40490a2cec3156c
luis Fernando Cardenas <luisfercb@gmail.com>
2019-05-17 08:42:43 -0500

    Adicon de corchete al final

--- My.js
+++ My.js
@@ -2,7 +2,7 @@
 var elColor = document.getElementById("esteColor");
 // Evento de click en boton
 var elBoton = document.getElementById("boton");
-
+// Se adiciono el EventListener
 elBoton.addEventListener("click", funcionEscriba);
 
 function funcionEscriba() {
